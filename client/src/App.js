1b48bb232b679b9a4decae2eaeab5225c1f21bc4
Darth Vader <danieltarancon@gmail.com>
2023-12-19 20:49:58 +0100

    19 2049

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch the list of courses from your API
+    async function fetchCourses() {
+      try {
+        const response = await fetch('http://localhost:5001/api/courses'); // Update the URL as needed
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setCourses(data);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    }
+
+    fetchCourses();
+  }, []); // Empty dependency array to run once when the component mounts
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>List of Courses:</p>
+        <ul>
+          {courses.map((course) => (
+            <li key={course.id}>{course.title}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
