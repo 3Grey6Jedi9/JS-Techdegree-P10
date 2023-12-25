692b44be503e757e836ed1487d2e79ff1d203a58
Darth Vader <danieltarancon@gmail.com>
2023-12-25 18:27:14 +0100

    25 1827

--- client/src/Courses.js
+++ client/src/Courses.js
@@ -1,32 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Courses() {
-    const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState([]);
 
-    useEffect(() => {
-
-        // Function to fetch the list of courses from your API
-        async function fetchCourses(){
-            try {
-                const response = await fetch('http://localhost:5001/api/courses');
-                if (!response.ok){
-                    throw new Error('Network response was not ok');
-
-                }
-                const data = await response.json();
-                setCourses(data);
-
-            } catch (error) {
-
-                console.error('Error fetching courses:', error);
-
-            }
-
+  useEffect(() => {
+    // Function to fetch the list of courses from your API
+    async function fetchCourses() {
+      try {
+        const response = await fetch('http://localhost:5001/api/courses'); // Update the URL as needed
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
+        const data = await response.json();
+        setCourses(data);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    }
 
-        fetchCourses();
+    fetchCourses();
+  }, []); // Empty dependency array to run once when the component mounts
 
-    }, []); // Empty depence
-
-
+  return (
+    <div>
+      <h2>Courses</h2>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>
+            <a href={`/courses/${course.id}`}>{course.title}</a>
+          </li>
+        ))}
+      </ul>
+      <a href="/create-course">Create Course</a>
+    </div>
+  );
 }
+
+export default Courses;
+
+
+
