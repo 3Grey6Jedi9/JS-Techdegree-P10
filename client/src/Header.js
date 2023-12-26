c0e87aaf252601b608577765aec41f03d0205907
Darth Vader <danieltarancon@gmail.com>
2023-12-26 18:06:43 +0100

    26 1806

---
+++ client/src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header({ authenticatedUser, signOut }) {
+  return (
+    <div className="header">
+      <div className="bounds">
+        <h1 className="header--logo">Courses</h1>
+        <nav>
+          {authenticatedUser ? (
+            <React.Fragment>
+              <span>Welcome, {authenticatedUser.firstName}!</span>
+              <Link className="signout" to="/signout" onClick={signOut}>
+                Sign Out
+              </Link>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Link className="signup" to="/signup">
+                Sign Up
+              </Link>
+              <Link className="signin" to="/signin">
+                Sign In
+              </Link>
+            </React.Fragment>
+          )}
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
