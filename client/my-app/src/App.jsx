a5c66a8ef9c1ba7bf8a44655d3db9e8264a03151
Darth Vader <danieltarancon@gmail.com>
2023-12-27 20:00:34 +0100

    27 2000

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
-import AppRoutes from './Routes';
+import React, { useEffect, useState } from 'react';
+import logo from './assets/logo.svg';
+import './styles/App.css';
+import Routes from './Routes';
+import axios from 'axios'; // Import Axios
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch the list of courses from your API
+    async function fetchCourses() {
+      try {
+        const response = await axios.get('http://localhost:5001/api/courses');
+        setCourses(response.data); // Use response.data to set courses
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    }
+
+    fetchCourses();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-      <AppRoutes />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>List of Courses:</p>
+        <Routes courses={courses} /> {/* Pass the courses data as a prop */}
+      </header>
     </div>
   );
 }
