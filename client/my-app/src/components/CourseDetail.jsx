50d59fae183b4df8be0679b0874f0fa30193a3a5
Darth Vader <danieltarancon@gmail.com>
2024-01-11 17:26:41 +0100

    11 1726

--- client/my-app/src/components/CourseDetail.jsx
+++ client/my-app/src/components/CourseDetail.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom'; // Import useParams
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
-import {useAuth} from "../AuthContext.jsx";
-
+import { useAuth } from '../AuthContext.jsx';
 
 function CourseDetail() {
   const [course, setCourse] = useState(null);
-  const { id } = useParams(); // Use the useParams hook to access route parameters
-  const {signOut} = useAuth();
+  const { id } = useParams();
+  const { user, signOut } = useAuth(); // Access the authenticated user and signOut function
+  const [isCourseOwner, setIsCourseOwner] = useState(false); // State to track if the user is the course owner
 
   useEffect(() => {
     // Function to fetch the course details from your API
     async function fetchCourseDetail() {
-  try {
-    const response = await axios.get(`http://localhost:5001/api/courses/${id}`);
-    if (response.status === 200) {
-      const data = response.data;
-      setCourse(data);
-    } else {
-      console.error(`Network response was not ok. Status: ${response.status}`);
+      try {
+        const response = await axios.get(`http://localhost:5001/api/courses/${id}`);
+        if (response.status === 200) {
+          const data = response.data;
+          setCourse(data);
+          console.log(user.id)
+          // Check if the authenticated user's ID matches the course owner's ID
+          setIsCourseOwner(user && user.id === data.userId);
+        } else {
+          console.error(`Network response was not ok. Status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error('Error fetching course details:', error);
+      }
     }
-  } catch (error) {
-    console.error('Error fetching course details:', error);
-  }
-}
 
     fetchCourseDetail();
-  }, [id]); // Fetch details when the 'id' parameter changes
+  }, [id, user]); // Fetch details when the 'id' parameter changes or when the user changes
 
   const handleDeleteCourse = async () => {
     try {
@@ -42,9 +45,8 @@ function CourseDetail() {
   };
 
   const handleSignOut = () => {
-    signOut(); // Calling the signOut function to sign the user out
+    signOut();
   };
-
 
   return (
     <div>
@@ -53,8 +55,12 @@ function CourseDetail() {
           <h2>Course Detail</h2>
           <h3>{course.title}</h3>
           <p>{course.description}</p>
-          <button onClick={handleDeleteCourse}>Delete Course</button>
-          <Link to="update">Update Course</Link>
+          {user && isCourseOwner && ( // Check if there's an authenticated user and if they are the course owner
+            <div>
+              <button onClick={handleDeleteCourse}>Delete Course</button>
+              <Link to="update">Update Course</Link>
+            </div>
+          )}
           <button onClick={handleSignOut}>Sign Out</button>
         </div>
       ) : (
