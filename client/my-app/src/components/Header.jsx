99bde8fafca32cec0ef650828d49271ef44ed5f3
Darth Vader <danieltarancon@gmail.com>
2024-01-16 12:13:30 +0100

    16 1213

--- client/my-app/src/components/Header.jsx
+++ client/my-app/src/components/Header.jsx
@@ -3,26 +3,32 @@ import { Link } from 'react-router-dom';
 
 function Header({ authenticatedUser, signOut }) {
   return (
-    <div className="header">
-      <div className="bounds">
-        <h1 className="header--logo">Courses App</h1>
+    <div className="header header--flex">
+      <div className="bounds wrap">
+        <h1 className="header--logo"><a href="/">Courses App</a></h1>
         <nav>
           {authenticatedUser ? (
-            <React.Fragment>
-              <span>Welcome, {authenticatedUser.firstName}!</span>
-              <Link className="signout" to="/signout" onClick={signOut}>
-                Sign Out
-              </Link>
-            </React.Fragment>
+            <ul className="header--signedin">
+              <li>Welcome, {authenticatedUser.firstName}!</li>
+              <li>
+                <Link className="signout button" to="/signout" onClick={signOut}>
+                  Sign Out
+                </Link>
+              </li>
+            </ul>
           ) : (
-            <React.Fragment>
-              <Link className="signup" to="/signup">
-                Sign Up
-              </Link>
-              <Link className="signin" to="/signin">
-                Sign In
-              </Link>
-            </React.Fragment>
+            <ul className="header--signedout">
+              <li>
+                <Link className="signup button" to="/signup">
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link className="signin button" to="/signin">
+                  Sign In
+                </Link>
+              </li>
+            </ul>
           )}
         </nav>
       </div>
